a9dff63c46fc1e8496efb9694175305e1d804240
Geoffroy Van Elsuve <geoffroy@vanelsuve.info>
2018-01-15 21:28:27 +0100

    Update stdint.h

--- arduino/cores/stdint.h
+++ arduino/cores/stdint.h
@@ -30,7 +30,7 @@ typedef short  int16_t;
 typedef unsigned short  uint16_t;
 typedef int  int32_t;
 typedef unsigned   uint32_t;
-//typedef long long  int64_t;
+typedef long long  int64_t;
 typedef unsigned long long   uint64_t;
 
 /* 7.18.1.2  Minimum-width integer types */
